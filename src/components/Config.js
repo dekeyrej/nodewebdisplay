b81bcaa273a3d1bc52c2d18af091f81ff742a519
Joe DeKeyrel <joedekeyrel@gmail.com>
2025-01-17 20:44:41 -0500

    Resolve eqeqeq error

--- src/components/Config.js
+++ src/components/Config.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 let Config
 
 // rassafrassing fs.readFileSync uses paths relative to project root?!?!?!
-if (process.env.DEV == 1) {
+if (process.env.DEV === 1) {
     Config = JSON.parse(fs.readFileSync('./config/secrets.dev.json', 'utf-8'));
 } else {
     Config = JSON.parse(fs.readFileSync('./config/secrets.json', 'utf-8'));
