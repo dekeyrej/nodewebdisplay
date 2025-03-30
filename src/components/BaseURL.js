adc73640e92fd1e2e4e49a925958d86723b19e7e
Joe DeKeyrel <joedekeyrel@gmail.com>
2025-03-30 14:54:52 -0400

    Update BaseURL for mapped ingress

--- src/components/BaseURL.js
+++ src/components/BaseURL.js
@@ -1,6 +1,4 @@
-// const api_port = 3002
-
-// const BaseURL = `http://${window.location.hostname}:${api_port}`;
+// This file is used to set the base URL for the API requests
 const BaseURL = `http://${window.location.hostname}/api`;
 console.log(BaseURL);
 
