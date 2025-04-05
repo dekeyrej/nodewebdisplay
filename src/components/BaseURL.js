c10f6d8dcf5fb644d4aa6351960895ad5374ea88
dekeyrej <74379607+dekeyrej@users.noreply.github.com>
2025-04-05 16:00:28 -0400

    Update BaseURL.js

--- src/components/BaseURL.js
+++ src/components/BaseURL.js
@@ -1,6 +1,12 @@
 // This file is used to set the base URL for the API requests
-const BaseURL = `http://${window.location.hostname}/api`;
-// const BaseURL = `http://rocket2/api`;
+let BaseURL = '';
+if (process.env.REACT_APP_DEV === '1') {
+  // If in development mode, use the localhost URL
+  BaseURL = `http://rocket2/api`;
+} else {
+  // If in production mode, use the production URL
+  BaseURL = `http://${window.location.hostname}/api`;
+}
 console.log(BaseURL);
 
 export default BaseURL
