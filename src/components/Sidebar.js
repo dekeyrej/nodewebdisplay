83b9a0b37160c973e71633c3c2998c316841279e
dekeyrej <joedekeyrel@gmail.com>
2025-04-07 11:46:32 -0400

    Updated sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,7 +10,7 @@ export default function Sidebar() {
                 <Nav.Link href="/mlb">MLB</Nav.Link>
                 <Nav.Link><hr></hr></Nav.Link>
                 <Nav.Link href="/mm/">MagicMirror</Nav.Link>
-                <Nav.Link href="/mm2/">MagicMirror-small screen</Nav.Link>
+                <Nav.Link href="/mm2/">MagicMirror-small</Nav.Link>
                 <Nav.Link href={`https://${window.location.hostname}/dashboard/#/`}>Cluster Dashboard</Nav.Link>
                 <Nav.Link href={`http://${window.location.hostname}:8080`}>Open-WebUI</Nav.Link>
                 <Nav.Link href={`http://${window.location.hostname}:3000`}>Grafana</Nav.Link>
